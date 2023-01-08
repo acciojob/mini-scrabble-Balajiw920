463f9c153324cb01e721f52be3058c7b5fd5f9e5
Balajiw920 <wbalaji2021@gmail.com>
2023-01-08 20:44:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+const textbox = document.getElementById('evaluatedText');
+const letterCount = document.getElementById('letterCount');
+
+textbox.addEventListener('keyup', () => {
+  letterCount.textContent = textbox.value.length;
+});
+
